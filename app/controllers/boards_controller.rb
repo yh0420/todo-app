8387b90478a95e33e760a3c64dd39eabe45c4056
yuta higa <yh19950420@gmail.com>
2021-10-04 19:33:14 +0900

    新規投稿画面を作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @boards = Board.all
   end
@@ -7,8 +8,25 @@ class BoardsController < ApplicationController
   end
 
   def create
-    @board.save
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to root_path(@board), notice: '保存できたよ'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+  
+  def new
+    @board = current_user.boards.build
   end
 end
 
-#@board = Board.first
+
+private
+def board_params
+  params.require(:board).permit(:title, :content)
+end
+def set_board
+  @board = Board.find(params[:id])
+end
